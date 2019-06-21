554df04e5cb1a5baa44c96eecd8e7d7d615cc8c5
J Kishan <51126867+KishanJ29@users.noreply.github.com>
2019-06-21 23:38:54 +0530

    Sequential Multiplexer

---
+++ mux.vhd
@@ -0,0 +1,22 @@
+library ieee;
+    use ieee.std_logic_1164.all;
+    entity mux is
+        port(s:in std_logic_vector(1 downto 0);
+            i:in std_logic_vector(3 downto 0);
+            y : out std_logic);
+        end mux;
+        architecture behav of mux is
+            begin
+                
+               process(s)
+               begin 
+                  case s is
+                  when "00" => y <= i(0);
+                  when "01" => y <= i(1);
+                  when "10" => y <= i(2);
+                  when others => y <= i(3);
+              end case;
+          end process; 
+          
+            end behav;
+
